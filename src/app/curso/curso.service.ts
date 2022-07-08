62a9b3a60045745b09c1181a0adb1066cf43b16d
Maisa Matos <maisamatos5@gmail.com>
2022-07-08 18:48:28 -0300

    first commit

---
+++ src/app/curso/curso.service.ts
@@ -0,0 +1,83 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators'
+import { Curso } from './curso';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CursoService {
+
+  //URL
+  url = 'http://localhost/api/php/';
+
+  //Vetor
+  //tive que inicializar o vetor, ao contrário do instrutor
+  vetor: Curso[] = [];
+  
+
+  constructor(private http: HttpClient) { }
+
+  //obter todos os cursos
+  //tive que colocar o res como tipo any, pois ele não reconhece 'cursos'
+  obterCursos():Observable<Curso[]> {
+    return this.http.get(this.url +'listar').pipe(
+      map((res: any) => {
+        this.vetor = res['cursos'];
+        return this.vetor;
+      })
+    )
+  }
+
+  //cadastrar curso
+  //tive que colocar o res como tipo any, pois ele não reconhece 'cursos'
+  cadastrarCurso(c:Curso): Observable<Curso[]>{
+    return this.http.post(this.url+'cadastrar', {cursos:c})
+    .pipe(map((res: any) => {
+      this.vetor.push(res['cursos']);
+      return this.vetor;
+    }))
+  }
+
+  removerCurso(c:Curso): Observable<Curso[]>{
+
+    const params = new HttpParams().set("idCurso", c.idCurso?.toString());
+
+    return this.http.delete(this.url+'excluir', {params: params})
+    .pipe(map((res) => {
+
+      const filtro = this.vetor.filter((curso) => {
+        return +curso['idCurso'] !== +c.idCurso;
+      });
+
+      return this.vetor = filtro;
+
+    }))
+
+  }
+
+  atualizarCurso(c:Curso): Observable<Curso[]>{
+    //executa a alteração via url
+    return this.http.put(this.url+'alterar', {cursos: c})
+    
+    //percorre o vetor para saber o id do curso alterado
+    .pipe(map((res) => {
+      const cursoAlterado = this.vetor.find((item) => {
+        return +item['idCurso'] === +['idCurso'];
+      });
+
+      //ao encontrar, altero o valor do vetor local
+      if(cursoAlterado){
+        cursoAlterado['nomeCurso'] = c['nomeCurso'];
+        cursoAlterado['valorCurso'] = c['valorCurso'];
+      }
+
+      //retorno
+      return this.vetor;
+
+    }))
+
+  }
+
+}
